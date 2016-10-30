eea39aa9aa9cc4c532314af37ac85ebf551b6d26
Nati Tessema <nati.tessema@gmail.com>
2016-10-29 20:09:35 -0700

    Fixing module import (#49)

--- src/client/app/+graph/graph.module.ts
+++ src/client/app/+graph/graph.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GraphComponent } from './graph.component';
+import { SharedModule } from '../shared/shared.module';
+
 
 @NgModule({
-    imports: [CommonModule],
+    imports: [CommonModule, SharedModule],
     declarations: [GraphComponent],
     exports: [GraphComponent]
 })
