68d42f289474d103ba070bfc678c18ce5b6cf0e5
Eyuel Tessema <eyueltessema@gmail.com>
2016-11-05 19:21:35 -0700

    Change canvas coordinate space origin to top left. Closes #70

--- src/client/app/shared/canvas/canvas.service.ts
+++ src/client/app/shared/canvas/canvas.service.ts
@@ -14,14 +14,14 @@ export type ViewportCoord = Point;
 // A point in the coordinate system of the canvas.
 export type CanvasCoord = Point;
 
-/**
+/*
  * The CanvasService maintains the data of the cards to show on the canvas,
  * handles zooming and panning on the canvas.
  * Zooming grows and shrinks the viewport relative to the canvas and panning
  * translates the viewport relative to the canvas.
  * This class also handles converting between the viewport's coordinate system,
- * the size of which is specified by viewportSize, and the canvas' coordinate
- * system, which is infinitely large.
+ * the size of which is finite, and the canvas' coordinate system, which is
+ * infinitely large.
  * The viewport coord system's origin is at the top left of the viewport.
  * The canvas coord system's origin starts off at the center of the viewport.
  */
@@ -33,11 +33,8 @@ export class CanvasService {
   cards: Card[] = [];
   // The zoom scale relative to the original viewport size.
   zoomScale: number = 1;
-  // The offset from the original viewport position.
-  centerOffset: CanvasCoord = {x:0, y:0};
-  // TODO(eyuelt): get this programmatically
-  // The size of the viewport. Must match the sizes specified in html and css.
-  viewportSize = {w:800, h:800};
+  // The offset of the viewport from its original position.
+  originOffset: CanvasCoord = {x:0, y:0};
   // TODO(eyuelt): I think I can use RXJS' Subject for this?
   // The list of functions to call when zoom or pan occurs. This is used to
   // essentially watch this class' properties.
@@ -45,25 +42,25 @@ export class CanvasService {
 
   constructor() {
     // TODO(eyuelt): fetch this card data from wherever it's stored
-    this.cards.push(new Card(-100, -100, 20));
     this.cards.push(new Card(0, 0, 20));
-    this.cards.push(new Card(20, 20, 20));
     this.cards.push(new Card(60, 60, 20));
+    this.cards.push(new Card(100, 100, 20));
+    this.cards.push(new Card(200, 200, 20));
   }
 
   // Convert a point in the viewport coordinate space to a point in the canvas
   // coordinate space.
   viewportCoordToCanvasCoord(vp: ViewportCoord): CanvasCoord {
-    const x = (vp.x - this.viewportSize.w/2) / this.zoomScale + this.centerOffset.x;
-    const y = (vp.y - this.viewportSize.h/2) / this.zoomScale + this.centerOffset.y;
+    const x = vp.x / this.zoomScale + this.originOffset.x;
+    const y = vp.y / this.zoomScale + this.originOffset.y;
     return {x, y};
   }
 
   // Convert a point in the canvas coordinate space to a point in the viewport
   // coordinate space.
   canvasCoordToViewportCoord(cv: CanvasCoord): ViewportCoord {
-    const x = (cv.x - this.centerOffset.x) * this.zoomScale + this.viewportSize.w/2;
-    const y = (cv.y - this.centerOffset.y) * this.zoomScale + this.viewportSize.h/2;
+    const x = (cv.x - this.originOffset.x) * this.zoomScale;
+    const y = (cv.y - this.originOffset.y) * this.zoomScale;
     return {x, y};
   }
 
@@ -72,8 +69,8 @@ export class CanvasService {
   zoom(zoomPnt: ViewportCoord, incZoomScale: number) {
     const toZoom = this.zoomScale * incZoomScale;
     if (toZoom > this.kMinZoomScale && toZoom < this.kMaxZoomScale) {
-      this.centerOffset.x += (zoomPnt.x - this.viewportSize.w/2) * (1 - (1 / incZoomScale)) / this.zoomScale;
-      this.centerOffset.y += (zoomPnt.y - this.viewportSize.h/2) * (1 - (1 / incZoomScale)) / this.zoomScale;
+      this.originOffset.x += zoomPnt.x * (1 - (1 / incZoomScale)) / this.zoomScale;
+      this.originOffset.y += zoomPnt.y * (1 - (1 / incZoomScale)) / this.zoomScale;
       this.zoomScale = toZoom;
       this.notifyListeners();
     }
@@ -81,8 +78,8 @@ export class CanvasService {
 
   // Pan the canvas by the vector specified by delta.
   pan(delta: ViewportCoord) {
-    this.centerOffset.x += delta.x / this.zoomScale;
-    this.centerOffset.y += delta.y / this.zoomScale;
+    this.originOffset.x += delta.x / this.zoomScale;
+    this.originOffset.y += delta.y / this.zoomScale;
     this.notifyListeners();
   }
 
