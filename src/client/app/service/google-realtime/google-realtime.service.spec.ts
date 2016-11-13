3ea29f56e50ea167faf53ab4891c2ef0fbfc3e79
Girum Ibssa <girum.ibssa@gmail.com>
2016-11-13 15:18:43 -0800

    Build skeleton for google-realtime.service.spec.ts.

---
+++ src/client/app/service/google-realtime/google-realtime.service.spec.ts
@@ -0,0 +1,47 @@
+import {ReflectiveInjector} from '@angular/core';
+import {GoogleRealtimeService} from './google-realtime.service';
+export function main() {
+  describe('Google Realtime Service', () => {
+    let googleRealtimeService: GoogleRealtimeService;
+
+
+    beforeEach(() => {
+      let injector = ReflectiveInjector.resolveAndCreate([]);
+
+      googleRealtimeService = injector.get(GoogleRealtimeService);
+    });
+
+
+    describe('Authorization', () => {
+      it('should authorize with a popup', () => {
+
+      });
+
+      it('should authorize without a popup', () => {
+
+      });
+    });
+
+
+    describe('/ListFiles', () => {
+      it('should filter out files for MIME type uxgraph', () => {
+
+      });
+    });
+
+
+    describe('Realtime', () => {
+      it('should load the Realtime Model', () => {
+
+      });
+    });
+
+
+    describe('/CreateFile', () => {
+      it('should create files on Google Drive of MIME type uxgraph', () => {
+
+      });
+    });
+
+  });
+}
