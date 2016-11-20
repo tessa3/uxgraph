6bf55bf978a921bf0063b4830aba6bb15388e9f7
Girum Ibssa <girum.ibssa@gmail.com>
2016-11-20 14:50:14 -0800

    Added <uxg-secondary-toolbar> with some basic styles. Next up is fake buttons.

--- src/client/app/+graph/graph.module.ts
+++ src/client/app/+graph/graph.module.ts
@@ -1,13 +1,19 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { GraphComponent } from './graph.component';
-import { SharedModule } from '../shared/shared.module';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {GraphComponent} from './graph.component';
+import {SharedModule} from '../shared/shared.module';
+import {SecondaryToolbarComponent} from './secondary-toolbar/secondary-toolbar';
 
 
 @NgModule({
-    imports: [CommonModule, SharedModule],
-    declarations: [GraphComponent],
-    exports: [GraphComponent]
+  imports: [CommonModule, SharedModule],
+  declarations: [
+    GraphComponent,
+    SecondaryToolbarComponent
+  ],
+  exports: [
+    GraphComponent
+  ]
 })
-
-export class GraphModule { }
+export class GraphModule {
+}
---
+++ src/client/app/+graph/secondary-toolbar/secondary-toolbar.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'uxg-secondary-toolbar',
+  templateUrl: 'secondary-toolbar.html',
+  styleUrls: ['secondary-toolbar.css']
+})
+export class SecondaryToolbarComponent {
+}
