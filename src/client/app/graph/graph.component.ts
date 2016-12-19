25345dbfd395bc5337eed3056e67ea515353af3b
Girum Ibssa <girum.ibssa@gmail.com>
2016-12-18 18:37:48 -0800

    Cleaned a little code.

--- src/client/app/graph/graph.component.ts
+++ src/client/app/graph/graph.component.ts
@@ -13,23 +13,14 @@ import {GoogleRealtimeService} from '../service/google-realtime.service';
 })
 export class GraphComponent implements OnInit {
 
-  // TODO(girum): Each activity should not maintain its own "userLoggedIn"
-  // state. This should get extracted out into its own singleton satte.
-  userLoggedIn: boolean = false;
   graphId: string;
 
   constructor(private googleRealtimeService: GoogleRealtimeService,
-              private route: ActivatedRoute) {
-    this.googleRealtimeService.oauthToken
-        .subscribe((oauthToken) => {
-          if (!!oauthToken) {
-            this.userLoggedIn = true;
-          }
-        });
+              private activatedRoute: ActivatedRoute) {
   }
 
   ngOnInit() {
-    this.route.params.forEach((params: Params) => {
+    this.activatedRoute.params.forEach((params: Params) => {
       this.graphId = params['graphId'];
       this.googleRealtimeService.loadRealtimeDocument(this.graphId);
     });
