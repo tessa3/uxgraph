f00ea73f3e74d5a9ef96e41b6dd6a685ac740cb6
Eyuel Tessema <eyueltessema@gmail.com>
2017-01-08 18:40:09 -0800

    Add a card to canvas on fab click

--- src/client/app/graph/graph.component.ts
+++ src/client/app/graph/graph.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Params} from '@angular/router';
 import {GoogleRealtimeService} from '../service/google-realtime.service';
+import {Card} from '../model/card';
 
 /**
  * This class represents the lazy loaded GraphComponent.
@@ -23,6 +24,20 @@ export class GraphComponent implements OnInit {
     this.activatedRoute.params.forEach((params: Params) => {
       this.graphId = params['graphId'];
       this.googleRealtimeService.loadRealtimeDocument(this.graphId);
+    });
+  }
+
+  //TODO(eyuelt): this is a temporary hack for adding cards to canvas
+  addCard() {
+    this.googleRealtimeService.currentDocument.first().subscribe((currentDocument) => {
+      let model = currentDocument.getModel();
+      let numCards = model.getRoot().get('cards').length;
+      let card = model.create(Card);
+      card.x = 10 + numCards * 5;
+      card.y = 10 + numCards * 5;
+      card.text = 'card #' + numCards;
+      card.selected = false;
+      model.getRoot().get('cards').push(card);
     });
   }
 
