38a565085eb57719397ac000f544000fc32abd55
Eyuel Tessema <eyueltessema@gmail.com>
2017-05-13 18:28:26 -0700

    Add missing typings

--- tools/manual_typings/project/gapi.drive.realtime.custom.d.ts
+++ tools/manual_typings/project/gapi.drive.realtime.custom.d.ts
@@ -4,4 +4,6 @@ declare namespace gapi.drive.realtime.custom {
   export function registerType(
       customCollaborativeType: any,
       typename: string): void;
+
+  export function setInitializer(type: any, initializerFn: () => void): void;
 }
